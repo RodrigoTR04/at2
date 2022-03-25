98eddf172b7ab2dbe162c4e219bbe387147f2e48
enzolsmoura <enderenzo@gmail.com>
2022-03-25 15:47:06 -0300

    ex1 resolvido, ex2 em andamento

--- src/ex1.c
+++ src/ex1.c
@@ -28,20 +28,89 @@ void exibe_vetor(int vetor[MAX_ARR], int tam_vetor);
 
 int main (int argc, char *argv[])
 {
-
+  int vetor[MAX_ARR],a,metodo,crescente;
+  a = le_vetor(vetor);
+  printf("Selecione o método de ordenação (1 - Selection, 2 - Bubble): ");
+  scanf("%i",&metodo);
+  printf("Ordenação crescente (1) ou decrescente (0): ");
+  scanf("%i",&crescente);
+  if(metodo==1){
+    selection_sort(vetor,a,crescente);
+  }
+  if(metodo==2){
+    bubble_sort(vetor,a,crescente);
+  }
+  exibe_vetor(vetor,a);
   return 0;
 }
 
 void troca_elementos(int vetor[MAX_ARR], int i, int j) {
+  int aux;
+  aux = vetor[i];
+  vetor[i] = vetor[j];
+  vetor[j] = aux;
 }
 
 void selection_sort(int vetor[MAX_ARR], int tam_vetor, int crescente) {
+  int i, j, min_ind,max_ind;
+  if(crescente==1){
+    for (i = 0; i < tam_vetor-1; i++){
+        min_ind = i;
+        for (j = i+1; j < tam_vetor; j++){
+          if (vetor[j] < vetor[min_ind]){
+            min_ind = j;
+          }  
+        }        
+        troca_elementos(vetor,min_ind,i);
+    }
+  }
+  if(crescente==0){
+    for (i = 0; i < tam_vetor-1; i++){
+        max_ind = i;
+        for (j = i+1; j < tam_vetor; j++){
+          if (vetor[j] > vetor[max_ind]){
+            max_ind = j;
+          }  
+        }        
+        troca_elementos(vetor,max_ind,i);
+    }
+  } 
 }
 
 void bubble_sort(int vetor[MAX_ARR], int tam_vetor, int crescente) {
+  int n,k;
+  if(crescente==1){
+    for(n=0;n<tam_vetor;n++){
+      for(k=0;k<tam_vetor-1;k++){
+        if(vetor[k]>vetor[k+1]){
+          troca_elementos(vetor,k,k+1);
+        }
+      }
+    }
+  }
+  if(crescente==0){
+    for(n=0;n<tam_vetor;n++){
+      for(k=0;k<tam_vetor-1;k++){
+        if(vetor[k]<vetor[k+1]){
+          troca_elementos(vetor,k,k+1);
+        }
+      }
+    }
+  }
 }
 
 int le_vetor(int vetor[MAX_ARR]) {
+  int i, a;
+  printf("Digite os valores do vetor: ");
+  for(i=0;i<MAX_ARR;i++){
+    scanf("%i",&a);
+    if(a==-100||i==MAX_ARR){
+      break;
+    } 
+    vetor[i] = a;
+  }
+  
+  return i;
 }
 
 void exibe_vetor(int vetor[MAX_ARR], int tam_vetor) {
--- src/ex2.c
+++ src/ex2.c
@@ -4,8 +4,31 @@
 
 void exibe_vetor(int vetor[ARR_SIZE]);
 
+void bubble_sort(int vetor[ARR_SIZE], int tam_vetor);
+
+int le_vetor(int vetor[ARR_SIZE]);
+
+void troca_elementos(int vetor[ARR_SIZE], int i, int j);
+
+int busca_binaria(int vetor[ARR_SIZE], int chave);
+
 int main (int argc, char *argv[])
 {
+  int a,b,c,chave,i,vetor[ARR_SIZE],matriz_aux[ARR_SIZE][2];
+  a = le_vetor(vetor);
+  printf("Digite a chave a ser buscada: ");
+  scanf("%i",&chave);
+  for(i=0;i<ARR_SIZE;i++){
+    matriz_aux[i][0] = vetor[i];
+    matriz_aux[i][1] = i;
+  }
+  bubble_sort(vetor,a);
+  b = busca_binaria(vetor,chave);
+  if(b!=-1){  
+    printf("Valor %i encontrado no índice %i.",chave,matriz_aux[][]);
+    return 0;
+  }
+  printf("Valor %i não foi encontrado!",chave);
 
   return 0;
 }
@@ -17,4 +40,53 @@ void exibe_vetor(int vetor[ARR_SIZE]) {
     if ( i < ARR_SIZE - 1 ) printf(", ");
   }
   printf("]\n");
+}
+
+int busca_binaria(int vetor[ARR_SIZE], int chave){
+  int i,pos_ini,pos_fim;
+  pos_ini = 0;
+  pos_fim = ARR_SIZE-1;
+  while(pos_ini<=pos_fim){
+    i = pos_ini + (pos_fim-pos_ini)/2;
+    if(vetor[i]==chave){
+      return i;
+    }
+    if(vetor[i]<chave){
+      pos_ini = i+1;
+    }
+    if(vetor[i]>chave){
+      pos_fim = i-1;
+    }
+  }
+  return -1;
+
+}
+
+void troca_elementos(int vetor[ARR_SIZE], int i, int j) {
+  int aux;
+  aux = vetor[i];
+  vetor[i] = vetor[j];
+  vetor[j] = aux;
+}
+
+void bubble_sort(int vetor[ARR_SIZE], int tam_vetor) {
+  int n,k;
+  for(n=0;n<tam_vetor;n++){
+    for(k=0;k<tam_vetor-1;k++){
+      if(vetor[k]>vetor[k+1]){
+        troca_elementos(vetor,k,k+1);
+      }
+    }
+  }
+}
+
+int le_vetor(int vetor[ARR_SIZE]) {
+  int i, a;
+  printf("Digite os valores do vetor: ");
+  for(i=0;i<ARR_SIZE;i++){
+    scanf("%i",&a);
+    vetor[i] = a;
+  }
+  
+  return i;
 }
